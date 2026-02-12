5f3847cab4d484bea94d9c86a99a298df1240aea
RoockDev <zar0_31@outlook.com>
2026-02-12 17:59:52 +0100

    feat(auth): implementar registro de un usuario y unificar enum roles

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,9 +1,16 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import type { Prisma } from 'generated/prisma/client';
+import { RoleName } from '../common/types/role-name.enum';
 import { PrismaService } from '../prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 type UserWithRoles = Prisma.UserGetPayload<{
   include: {
@@ -52,10 +59,54 @@ export class AuthService {
 
   async login(loginDto: LoginDto) {
     const user = await this.validateUser(loginDto.email, loginDto.password);
-    return this.buildAuthResponse(user);
+    return this.buildAuthResponse(user, 'Login correcto');
   }
 
-  private buildAuthResponse(user: SafeUserWithRoles) {
+  async register(registerDto: RegisterDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email: registerDto.email,
+      },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('El email ya está registrado');
+    }
+
+    const userRole = await this.prisma.role.findUnique({
+      where: {
+        name: RoleName.USER,
+      },
+    });
+
+    if (!userRole) {
+      throw new NotFoundException('No existe el rol USER');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: registerDto.email,
+        passwordHash: hashedPassword,
+        name: registerDto.name,
+        surname: registerDto.surname,
+        roles: {
+          connect: {
+            id: userRole.id,
+          },
+        },
+      },
+      include: {
+        roles: true,
+      },
+    });
+
+    const { passwordHash, ...safeUser } = user;
+    return this.buildAuthResponse(safeUser, 'Registro completado correctamente');
+  }
+
+  private buildAuthResponse(user: SafeUserWithRoles, message: string) {
     const roles = this.getNormalizedRoles(user.roles);
     const payload = {
       sub: user.id,
@@ -64,13 +115,13 @@ export class AuthService {
     };
 
     return {
-      message: 'Login correcto',
+      message,
       accessToken: this.jwtService.sign(payload),
       user: {
         id: user.id,
         email: user.email,
         name: user.name,
-        surnmae: user.surname,
+        surname: user.surname,
         roles,
       },
     };
